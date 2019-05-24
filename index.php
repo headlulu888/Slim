d75c8e2ac885b6b61969c68d563e62420879566f
headlulu888 <headlulu888@gmail.com>
2019-05-24 22:41:27 +0300

    Настройки и режимы работы Slim

--- index.php
+++ index.php
@@ -1,18 +1,55 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ray-pc
- * Date: 24/05/2019
- * Time: 21:22
- */
 
 require_once "vendor/autoload.php";
 
 \Slim\Slim::registerAutoloader();
 
-$app = new Slim\Slim();
+$app = new Slim\Slim([
+    'host' => 'localhost',
+    'user' => 'user',
+    'pass' => 'pass',
+    'db' => 'dbname',
 
-$app->get('/article', function () {
+    'mode' => 'development',
+    'debug' => TRUE,
+    'templates.path' => 'templates',
+
+    'cookies.encrypt' => TRUE,
+    'cookies.lifetime' => '20 minutes',
+    'cookies.path' => '/',
+    'cookies.domain' => 'slim', // current domain
+    'cookies.secure' => FALSE,
+    'cookies.httponly' => TRUE,
+    'cookies.secret_key' => 'key',
+    'cookies.cipher' => 'cipher',
+    'cookies.cipher_mode' => 'mode'
+]);
+
+$app->configureMode('development', function () use($app) {
+    $app->config([
+        'debug' => TRUE,
+        'templates.path' => "templates"
+    ]);
+});
+
+$app->configureMode('test', function () use($app) {
+    $app->config([
+        'debug' => FALSE,
+        'templates.path' => "templ"
+    ]);
+});
+
+//echo $app->config(array(
+//    'debug' => TRUE,
+//    'templates.path' => "templates"
+//));
+
+echo "<pre>";
+echo $app->config('templates.path');
+echo "</pre>";
+
+$app->get('/article', function () use($app) {
+
     echo 'World';
 });
 
