31a0ab3f076cf7abe7012a620156a2de5cb490f2
headlulu888 <headlulu888@gmail.com>
2019-05-25 22:46:05 +0300

    middleware

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 require_once "vendor/autoload.php";
 
 \Slim\Slim::registerAutoloader();
+\Slim\Route::setDefaultConditions(
+    [
+        'id' => '[1-9]+',
+        'name' => '[a-zA-Z]{3,}'
+    ]
+);
+
+// print_r(\Slim\Route::getDefaultConditions());
 
 $app = new Slim\Slim([
     'host' => 'localhost',
@@ -25,23 +33,52 @@ $app = new Slim\Slim([
     'cookies.cipher_mode' => 'mode'
 ]);
 
-$app->configureMode('development', function () use($app) {
-    $app->config([
-        'debug' => TRUE,
-        'templates.path' => "templates"
-    ]);
-});
-
-$app->configureMode('test', function () use($app) {
-    $app->config([
-        'debug' => FALSE,
-        'templates.path' => "templ"
-    ]);
-});
+//$app->configureMode('development', function () use($app) {
+//    $app->config([
+//        'debug' => TRUE,
+//        'templates.path' => "templates"
+//    ]);
+//});
+//
+//$app->configureMode('test', function () use($app) {
+//    $app->config([
+//        'debug' => FALSE,
+//        'templates.path' => "templ"
+//    ]);
+//});
 
 $app->get('/article(/:name(/:id))', function ($name = 'ray', $id = 1) use($app) {
     echo 'param = ' . $name . '|' . $id;
-});
+    $link = $app->urlFor('page', [
+        'id' => '50',
+        'name' => 'Sergey'
+    ]);
+
+    echo "<p><a href='" . $link . "'>URL</a></p>";
+
+})->name('page')->conditions([
+    'id' => '[a-zA-Z]{3,}'
+]);
+
+$getAdmin = function () {
+    echo "Hello admin";
+};
+
+//$authAdmin = function ($role = 'moderator') use($app) {
+//    return function () use($role, $app) {
+//        echo $role;exit();
+//        if ( 1 !== 10) {
+//            $app->redirect($app->urlFor('page'));
+//        }
+//    };
+//};
+
+$authAdmin = function (\Slim\Route $route) use($app) {
+    echo $route->getName();
+};
+
+// $app->get('/admin', $authAdmin('admin'), $getAdmin);
+$app->get('/admin', $authAdmin, $getAdmin)->name('admin');
 
 //$app->post('/add', function () {
 //    print_r($_POST);
